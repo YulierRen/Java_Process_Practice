b7df85d73131563f9e2267f115125f72725e1ce9
YulierRen <YulierRen2037@gmail.com>
2025-03-26 16:34:55 +0800

    Synchronized_Practice

--- src/YulierRen_Process_Practice/Counter/Counter.java
+++ src/YulierRen_Process_Practice/Counter/Counter.java
@@ -1,9 +1,13 @@
 package YulierRen_Process_Practice.Counter;
 
-import java.util.Scanner;
 
+/**
+ * {@code @Auther:} YulierRen
+ * 一个通过构造方法启动的sync测试类，对比两者的数据，体现sync的作用。
+ */
 public class Counter {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+//        new CounterTest_Sync();
+        new CounterTest_Nosync();
     }
 }
