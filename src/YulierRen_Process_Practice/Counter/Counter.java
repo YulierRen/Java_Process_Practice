bfb861227bb9ae1e659b546b927b3c920bb4d894
YulierRen <YulierRen2037@gmail.com>
2025-03-26 15:50:06 +0800

    测试

---
+++ src/YulierRen_Process_Practice/Counter/Counter.java
@@ -0,0 +1,9 @@
+package YulierRen_Process_Practice.Counter;
+
+import java.util.Scanner;
+
+public class Counter {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
